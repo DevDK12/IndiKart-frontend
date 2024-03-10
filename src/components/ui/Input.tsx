2e30259a2a9d52d23b1e1eb1477c2330c3355599
DevDK12 <devdk2208@gmail.com>
2024-03-10 14:59:58 +0530

    New Input UI variants

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -6,16 +6,17 @@ interface InputProps {
     type: string,
     placeholder?: string,
     value?: string | number,
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    variant?: 'FORM' | 'TEXTAREA' | 'FILE' | 'SEARCH',
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void ,
+    options?: string[],
+    variant?: 'FORM' | 'TEXTAREA' | 'FILE' | 'SEARCH' | 'DATE' | 'SELECT' ,
 }
 
 
 
-const Input = ({ label, type, placeholder, value, onChange, variant='FORM' }: InputProps) => {
+const Input = ({ label, type, placeholder, value, onChange, options, variant = 'FORM' }: InputProps) => {
 
-    
-    if(variant === "SEARCH") return (
+
+    if (variant === "SEARCH") return (
         <div className="flex flex-col gap-2 w-full">
             <input
                 className={`w-full rounded-md p-2  text-secondary-txt`}
@@ -27,17 +28,51 @@ const Input = ({ label, type, placeholder, value, onChange, variant='FORM' }: In
         </div>
     )
 
+    if (variant === "DATE") return (
+        <div className={`px-4 flex flex-col gap-2 sm:flex-row  sm:justify-between sm:items-center w-full`}>
+            {label && <label>{label}</label>}
+            <input
+                className={`w-full px-3 sm:w-2/3 rounded-md p-1 border-2 border-gray-400 focus:outline-none focus:border-cyan-400 text-secondary-txt`}
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    )
+
+    if (variant === 'SELECT') return (
+        <div className="px-4 flex flex-col gap-2 sm:flex-row  sm:justify-between sm:items-center w-full">
+            {label && <label>{label}</label>}
+            <select
+                className={`w-full px-3 sm:w-2/3 rounded-md p-1 border-2 border-gray-400 focus:outline-none focus:border-cyan-400 text-secondary-txt`}
+                value={value}
+                onChange={onChange}
+            >
+                <option value="" disabled>{placeholder}</option>
+                {options?.map((option, index) => (
+                    <option
+                        key={index}
+                        value={option}
+                    >{option}</option>
+                ))
+                }
+            </select>
+        </div>
+    )
+
+
     return (
-            <div className={`px-4 flex flex-col gap-2 sm:flex-row  sm:justify-between sm:items-center w-full`}>
-                {label && <label>{label}</label>}
-                <input
-                    className={`w-full sm:w-2/3 rounded-md p-1 border-2 border-gray-400 focus:outline-none focus:border-cyan-400 ${type === "file" ? "text-white" : 'text-secondary-txt'}`}
-                    type={type}
-                    placeholder={placeholder}
-                    value={value}
-                    onChange={onChange}
-                />
-            </div>
+        <div className={`px-4 flex flex-col gap-2 sm:flex-row  sm:justify-between sm:items-center w-full`}>
+            {label && <label>{label}</label>}
+            <input
+                className={`w-full px-3 sm:w-2/3 rounded-md p-1 border-2 border-gray-400 focus:outline-none focus:border-cyan-400 ${type === "file" ? "text-white" : 'text-secondary-txt'}`}
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
     )
 }
 
